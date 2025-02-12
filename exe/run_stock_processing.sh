6e92e65fe3e458ad148035b597d7d219b02b5367
hwet <ghlckd5424@gmail.com>
2025-02-12 13:50:16 +0900

    info.txt update

--- exe/run_stock_processing.sh
+++ exe/run_stock_processing.sh
@@ -17,15 +17,44 @@ fi
 
 echo "[INFO] 실행 시작 - 인자: $ARGS"
 
-# 가상환경 초기화 및 실행
+# Pyenv Setting
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init --path)"
-eval "$(pyenv init -)" 
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate hwetenv
 
-/home/hwechang_jeong/.pyenv/versions/3.9.16/bin/python /home/hwechang_jeong/stock/save_csv_stock_data.py $ARGS >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+# 주식 데이터 추출  (인자 포함)
+echo "[INFO] save_csv_stock_data.py 실행 중..."
+# /home/hwechang_jeong/.pyenv/versions/3.9.16/bin/python /home/hwechang_jeong/stock/save_csv_stock_data.py $ARGS >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
 
-/home/hwechang_jeong/.pyenv/versions/3.9.16/bin/python /home/hwechang_jeong/stock/csvToParquet.py
+python /home/hwechang_jeong/stock/save_csv_stock_data.py $ARGS >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+# 실행 결과 확인
+if [ $? -ne 0 ]; then
+    echo "[ERROR] save_csv_stock_data.py 실행 실패" >> /home/hwechang_jeong/stock/exe/stock_data.log
+    exit 1
+fi
 
+# CSV 파일 Parquet파일 변환
+echo "[INFO] csvToParquet.py 실행 중..."
+#/home/hwechang_jeong/.pyenv/versions/3.9.16/bin/python /home/hwechang_jeong/stock/csvToParquet.py >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+
+python /home/hwechang_jeong/stock/csvToParquet.py >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "[ERROR] csvToParquet.py 실행 실패" >> /home/hwechang_jeong/stock/exe/stock_data.log
+    exit 1
+fi
+
+# Parquet파일 DB 저장
+echo "[INFO] parquetToDB.py 실행 중..."
+#/home/hwechang_jeong/.pyenv/versions/3.9.16/bin/python /home/hwechang_jeong/stock/parquetToDB.py >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+
+python /home/hwechang_jeong/stock/parquetToDB.py >> /home/hwechang_jeong/stock/exe/stock_data.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "[ERROR] parquetToDB.py 실행 실패" >> /home/hwechang_jeong/stock/exe/stock_data.log
+    exit 1
+fi
 
 echo "[INFO] 실행 완료"
+
 
